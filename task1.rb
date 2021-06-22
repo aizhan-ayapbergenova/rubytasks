9ac8ba327d429548c7dc9dd47d6d2eebc8d245b0
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-06-22 11:32:13 +0600

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -1,8 +1,15 @@
+#Идеальный вес. Программа запрашивает у пользователя имя и рост и выводит идеальный вес по формуле <рост> - 110, 
+#после чего выводит результат пользователю на экран с обращением по имени. Если идеальный вес получается отрицательным, 
+#то выводится строка "Ваш вес уже оптимальный"
+
 puts "What is your name?"
 name = gets.chomp
+
 puts "What is your height?"
 height = gets.chomp.to_i
+
 ideal_height = height-110
+
 if ideal_height <=0
     puts "#{name}, your weight is ideal."
 else
