435f57b249c53953a48eec31bdbb81a0a239315c
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-06-29 20:59:39 +0600

    Update task2.rb

--- task 1/task2.rb
+++ task 1/task2.rb
@@ -1,11 +1,12 @@
 #Площадь треугольника. Площадь треугольника можно вычилсить, зная его основание (a) и высоту (h) по формуле: 1/2*a*h. 
 #Программа должна запрашивать основание и высоту треуголиника и возвращать его площадь.
 
-puts "Enter height"
-height = gets.chomp.to_f
+puts 'Enter base'
+a = gets.chomp.to_f
 
-puts "Enter base"
-base = gets.chomp.to_f
+puts 'Enter height'
+h = gets.chomp.to_f
 
-area = height*base*0.5
-puts "The area is equal to #{area}."
+area = a * h * 0.5
+
+puts "The triangular area is #{area}"
