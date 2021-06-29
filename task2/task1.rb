59cc0bfa12bff27313e788b5008882b8d4488968
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-06-29 21:33:28 +0600

    Update task1.rb

--- task2/task1.rb
+++ task2/task1.rb
@@ -1,6 +1,21 @@
 #Сделать хеш, содеращий месяцы и количество дней в месяце. В цикле выводить те месяцы, у которых количество дней ровно 30
+month = {
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july:31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
+}
 
-months = {january: 31, february: 28, march: 31, april: 30, may: 31, june: 30, july: 31, august: 31, september: 30, october: 31, november: 30, december: 31}
-months.each do |month, number|
-    puts "#{month}".inspect if number==30
-end
+month.each do |month, number|
+  if number == 30
+    puts "#{month}"
+  end
+end   
