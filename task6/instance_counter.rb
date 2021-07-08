dea26c7cff69de8fb02813e3492b757b55e92592
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-07-08 14:57:48 +0600

    Create instance_counter.rb

---
+++ task6/instance_counter.rb
@@ -0,0 +1,20 @@
+module InstanceCounter
+  def self.included(class_counter)
+    class_counter.extend ClassMethods
+    class_counter.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    attr_accessor :instances
+  end
+
+  module InstanceMethods
+
+    protected
+
+    def register_instance
+      self.class.instances ||= 0
+      self.class.instances += 1
+    end
+  end
+end
