18fbcfc222242e70d53f068d424816e89a56b015
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-07-08 15:00:14 +0600

    Create route.rb

---
+++ task6/route.rb
@@ -0,0 +1,33 @@
+class Route
+
+  attr_reader :stations
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def remove_station(station)
+    if station != first_station && station != last_station
+      @stations.delete(station)
+    end
+  end
+  
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    
+    if @stations[0].class.to_s != "Station" 
+      raise "The route must contain only stations" 
+    else
+      @stations[1].class.to_s != "Station"
+  end
+end
