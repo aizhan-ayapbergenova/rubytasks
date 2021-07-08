549b60bcf94721739c57de599b383e5327cb71ac
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-07-08 15:22:53 +0600

    Create cargo_wagon.rb

---
+++ task7/cargo_wagon.rb
@@ -0,0 +1,22 @@
+require_relative 'wagon'
+
+class CargoWagon < Wagon
+  
+  attr_reader :free_space, :filled_space
+  
+  def initialize(space)
+    @free_space = space
+    @taken_space = 0
+    raise "Space can not be empty or negative" if space == 0 || space < 0
+    super
+  end
+  
+  def fill_space(space)
+    raise "Available space is - #{@free_volume}" if space > @free_space
+    if @free_space> 0
+      @free_space -= space
+      @filled_space += space
+    else
+      puts "No free space"
+    end
+end
