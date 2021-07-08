1686c52b6d3f6c4d990d73071821224ec2fd76dc
aizhan-ayapbergenova <86282000+aizhan-ayapbergenova@users.noreply.github.com>
2021-07-08 15:27:10 +0600

    Create station.rb

---
+++ task7/station.rb
@@ -0,0 +1,45 @@
+class Station
+
+  attr_reader :trains, :name
+  @stations = []
+  ATTR_NAME = /^([a-z\d]\s?-?)*$/i
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+  
+  def self.all
+    @stations
+  end
+  
+  def train_check(&block)
+      @trains.each_with_index do |train, index|
+      block.call(train, index)
+    end
+  end
+
+  def take_train(train)
+    @trains.push(train) 
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def trains(type)
+    @trains.select { |train| train.class.to_s == type }
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    raise 'Enter the name of the station' if name.length.zero?
+    raise 'The name is incorrect' if name !~ ATTR_NAME
+  end
+end
